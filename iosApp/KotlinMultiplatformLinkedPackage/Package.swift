19af884ff65aba6701949fa7d10f671c626020ef
taetae98coding <taetae98coding@gmail.com>
2026-09-19 15:26:00 +0900

    fix: iOS 빌드를 복구한다
    9월 14일 버전 업 커밋들이 iOS 쪽 생성물에 반영되지 않아 빌드가 두 단계에서 깨졌다.
    
    firebase-ios-sdk는 Kotlin swiftPMDependencies와 Xcode 프로젝트가 서로 다른 버전을 exact로 고정해 SwiftPM이 패키지 그래프를 해석하지 못했다. Xcode 쪽을 12.19.1로 맞추고 재해석 결과를 Package.resolved에 반영한다.
    
    synthetic linkage 패키지는 Kotlin 2.4.20이 2.4.10과 다른 형태로 생성해 커밋된 산출물과 어긋났다. module.modulemap은 2.4.20이 새로 만드는 파일이다.

--- iosApp/KotlinMultiplatformLinkedPackage/Package.swift
+++ iosApp/KotlinMultiplatformLinkedPackage/Package.swift
@@ -14,16 +14,16 @@ let package = Package(
   ],
   dependencies: [
     .package(path: "subpackages/_feature_login_ui"),
-    .package(path: "subpackages/_compose_map"),
-    .package(path: "subpackages/_logger_crashlytics_impl")
+    .package(path: "subpackages/_logger_crashlytics_impl"),
+    .package(path: "subpackages/_compose_map")
   ],
   targets: [
     .target(
       name: "KotlinMultiplatformLinkedPackage",
       dependencies: [
         .product(name: "_feature_login_ui", package: "_feature_login_ui"),
-        .product(name: "_compose_map", package: "_compose_map"),
-        .product(name: "_logger_crashlytics_impl", package: "_logger_crashlytics_impl")
+        .product(name: "_logger_crashlytics_impl", package: "_logger_crashlytics_impl"),
+        .product(name: "_compose_map", package: "_compose_map")
       ]
     )
   ]
