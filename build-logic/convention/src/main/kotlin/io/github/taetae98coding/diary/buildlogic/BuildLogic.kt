79b926c5612284231d4b485f85fa853563e9fd02
taetae98coding <taetae98coding@gmail.com>
2026-09-25 11:13:37 +0900

    chore: 앱 빌드 번호를 2로 올린다

--- build-logic/convention/src/main/kotlin/io/github/taetae98coding/diary/buildlogic/BuildLogic.kt
+++ build-logic/convention/src/main/kotlin/io/github/taetae98coding/diary/buildlogic/BuildLogic.kt
@@ -6,6 +6,6 @@ public data object BuildLogic {
     internal const val TARGET_SDK = 37
 
     public const val NAMESPACE: String = "io.github.taetae98coding.diary"
-    public const val VERSION_CODE: Int = 1
+    public const val VERSION_CODE: Int = 2
     public const val VERSION_NAME: String = "2.0.0"
 }
